82102818d3c76ba72f27c3fa31ab7e52309816f4
leonardof108 <leonardof108@mail>
2024-11-13 12:21:30 -0300

    redesign

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    const toggleMenu = document.querySelector(".toggleMenu")
+    const navigation = document.querySelector(".navigation")
+    toggleMenu.classList.toggle("active")
+}
